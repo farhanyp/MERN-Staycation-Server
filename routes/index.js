809cf5ec0956642e191a6b482fb4352850c71932
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-06-02 19:15:40 +0700

    aa

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', function(req, res, next) {
-  res.redirect('/admin')
+  res.redirect('/admin/signin')
 });
 
 module.exports = router
