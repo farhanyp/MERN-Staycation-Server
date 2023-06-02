18a1f3bf61703f86266b3d9ede1191de792e17b8
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-06-02 20:27:00 +0700

    a

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,6 @@
 const isLogin = (req,res,next)=>{
     if(req.session.user === undefined){
-        req.flash('alertMessage', `${req.session.user}`)
+        req.flash('alertMessage', `${req.session}`)
         req.flash('alertStatus', 'danger')
         res.redirect('/admin/signin')
     }else{
