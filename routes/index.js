c87630884176360b5fcfa1640bd88629dd356c56
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-06-02 18:56:04 +0700

    add

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-router.get("/", async (req, res, next) => {
-  return res.status(200).json({
-    title: "Express Testing",
-    message: "The app is working properly!",
-  });
+router.get('/', function(req, res, next) {
+  res.redirect('/admin/signin')
 });
 
 module.exports = router
