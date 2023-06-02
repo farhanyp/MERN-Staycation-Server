ec40fee8b7981f6476052b917601d9d325f607cb
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-06-02 18:52:48 +0700

    check API

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', function(req, res, next) {
-    res.redirect('/admin/signin')
+router.get("/", async (req, res, next) => {
+  return res.status(200).json({
+    title: "Express Testing",
+    message: "The app is working properly!",
   });
+});
 
 module.exports = router
