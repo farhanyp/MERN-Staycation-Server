2b48f68bb16631b53aef1f9175490b13d231bdb1
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-06-02 18:57:37 +0700

    check

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const adminController = require('../controllers/adminController')
 const router = express.Router()
 
-router.get('/', function(req, res, next) {
-  res.redirect('/admin/signin')
-});
+router.get('/', adminController.viewSignIn)
+// router.get('/', function(req, res, next) {
+//   res.redirect('/admin/signin')
+// });
 
 module.exports = router
