394360b78db08bc003971aa59c6c0819aae1ded1
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-06-02 19:20:15 +0700

    aa

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@ const express = require('express')
 const adminController = require('../controllers/adminController')
 const router = express.Router()
 
-router.get("/", async (req, res, next) => {
+router.get("/signin", async (req, res, next) => {
   return res.status(200).json({
     title: "Express Testing",
     message: "The app is working properly!",
